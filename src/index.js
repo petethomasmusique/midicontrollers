14359b7c30c14d3e253ceb933140d3f7ea1fa591
Peter Thomas <you@example.com>
2017-10-09 12:39:40 +0100

    added redux library

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import state from "./data/reducer";
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// create a store using our reducer
+const store = createStore(state);
+
+ReactDOM.render(
+    // wrap the App component with the store Provider
+    <Provider store={ store }>
+        <App />
+    </Provider>,
+
+    document.getElementById("root")
+);
