290d6fa9ce5ebefd0e00be666c8f876498376b0f
Peter Thomas <you@example.com>
2017-11-10 10:06:40 +0000

    styling of app in prepartion for new components

--- src/data/initial.js
+++ src/data/initial.js
@@ -1,12 +1,11 @@
-import { Map, Range } from "immutable";
-// TODO - set a midiOut port - what to do if no port is available (currently if no port - MAX is not open for example - app will crash)
+import { Map, Range, List } from "immutable";
 
 export default Map({
 	midiInDevice: 'from Max 1',
 	midiOutDevice: 'to Max 1',
-	availableMidiDevices: '',
+	availableMidiDevices: List([]),
 	mouseDown: false,
-	knobs: Range(0, 8).map((i) => Map({ value: 0, label: 'send', channel: i })).toList(),
-	faders: Range(6, 15).map((i) => Map({ value: 0, midiValue: 127, label: 'level', channel: i })).toList(),
+	knobs: Range(9, 19).map((i) => Map({ value: 0, label: 'send', channel: i })).toList(),
+	faders: Range(0, 9).map((i) => Map({ value: 0, midiValue: 127, label: 'level', channel: i })).toList(),
 	sequencer: Range(0, 128).map(() => Map({velocity: 0, colour: '#DCDCDC'})).toList(),
 });
